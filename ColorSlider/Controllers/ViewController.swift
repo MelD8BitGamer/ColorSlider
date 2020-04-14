85460e555ad90c6a388439980205830f696d874d
melinda <melindad_82@msn.com>
2020-04-14 09:50:27 -0400

    first commit

---
+++ ColorSlider/Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ColorSlider
+//
+//  Created by Melinda Diaz on 4/14/20.
+//  Copyright © 2020 Melinda Diaz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
